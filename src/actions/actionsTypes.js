c03586e2647a7eb4e09a4e98a6456bceafed0449
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-11 18:03:19 +0200

    creating an actionTypes for the reducer capability

---
+++ src/actions/actionsTypes.js
@@ -0,0 +1,8 @@
+/** @format */
+
+export const actionsTypes = {
+  GET_DATA_BY_AUTHOR: "GET_DATA_BY_AUTHOR",
+  GET_AUTHOR: "GET_AUTHOR",
+  GET_ALBUM: "GET_ALBUM",
+  GET_ALBUM_ARTIST: "GET_ALBUM_ARTIST",
+};
