d41959cccb539fd55175f907bc0af540480f2b9f
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-12 15:51:34 +0200

    change of names to more appropriate

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,19 +2,19 @@
 
 import { actionsTypes } from "./actionsTypes";
 
-export const getDataFromApiByAuthorAction = (data) => ({
-  type: actionsTypes.GET_DATA_BY_AUTHOR,
+export const getDataFromApiByPhrase = (data) => ({
+  type: actionsTypes.GET_DATA_BY_PHRASE,
   payload: data,
 });
-export const getAuthorToStore = (author) => ({
+export const putAuthorToStore = (author) => ({
   type: actionsTypes.GET_AUTHOR,
   payload: author,
 });
-export const getDataAboutOneAlbum = (album) => ({
+export const getDataFromApiAboutOneAlbum = (album) => ({
   type: actionsTypes.GET_ALBUM,
   payload: album,
 });
-export const getDataAboutAlbumArtist = (data) => ({
+export const putDataAboutAlbumArtist = (data) => ({
   type: actionsTypes.GET_ALBUM_ARTIST,
   payload: data,
 });
