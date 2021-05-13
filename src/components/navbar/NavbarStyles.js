15b41c9573ac8b5e63dbe201e703b99119770fc8
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-13 19:34:34 +0200

    adding styles to navbar

---
+++ src/components/navbar/NavbarStyles.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const NavbarWrapper = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10vh;
+  width: 100vw;
+  background-color: #212a36;
+  border-bottom: 1px solid #388bfd;
+`;
