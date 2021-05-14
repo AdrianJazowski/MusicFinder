b31a96f6ac9d9349d9459b50e7bb6614a9dc993d
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-14 20:29:55 +0200

    breaking down components into smaller ones

--- src/views/artistPage/ArtistPage.js
+++ src/views/artistPage/ArtistPage.js
@@ -1,52 +1,33 @@
 /** @format */
 
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getDataFromApiByOneAlbum } from "../../api/apiCallFunctions";
+import { useSelector } from "react-redux";
+import AlbumsList from "../../components/albumsList/AlbumsList";
 import Popup from "../../components/popup/Popup";
 import {
   ArtistPageWrapper,
   ArtistInfo,
   AlbumsWrapper,
-  AlbumListUl,
 } from "./ArtistPageStyles";
 
 const ArtistPage = () => {
   const [openPopup, setOpenPopup] = useState(false);
   const selectedAuthor = useSelector((state) => state.author);
-  const selectedAlbums = useSelector((state) => state.authorAlbums);
-
-  const dispatch = useDispatch();
 
   const handleOpenDialogWithAlbumData = () => {
     setOpenPopup(!openPopup);
   };
-
-  const albumList = () => (
-    <AlbumListUl>
-      {selectedAlbums.map((album) => {
-        const { collectionName, collectionId } = album;
-        return (
-          <li
-            key={collectionId}
-            onClick={() => {
-              dispatch(getDataFromApiByOneAlbum(collectionId));
-              handleOpenDialogWithAlbumData();
-            }}
-          >
-            <p>{collectionName}</p>
-          </li>
-        );
-      })}
-    </AlbumListUl>
-  );
 
   return (
     <ArtistPageWrapper>
       <ArtistInfo>
         <h1>{selectedAuthor.toUpperCase()}</h1>
       </ArtistInfo>
-      <AlbumsWrapper>{albumList()}</AlbumsWrapper>
+      <AlbumsWrapper>
+        <AlbumsList
+          handleOpenDialogWithAlbumData={handleOpenDialogWithAlbumData}
+        />
+      </AlbumsWrapper>
       <Popup
         handleOpenDialogWithAlbumData={handleOpenDialogWithAlbumData}
         openPopup={openPopup}
--- src/views/artistPage/ArtistPageStyles.js
+++ src/views/artistPage/ArtistPageStyles.js
@@ -24,27 +24,3 @@ export const AlbumsWrapper = styled.div`
   display: flex;
   align-self: center;
 `;
-export const AlbumListUl = styled.ul`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  width: 90%;
-  list-style: none;
-  li {
-    color: #388bfd;
-    cursor: pointer;
-    &:hover {
-      color: #8799b1;
-    }
-    p {
-      margin: 0.2rem;
-
-      @media (max-width: 1024px) {
-        font-size: 16px;
-      }
-    }
-  }
-  @media (max-width: 769px) {
-    flex-wrap: nowrap;
-  }
-`;
